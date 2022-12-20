4890be1020723ce2a3b9a5ca060f3e16872539cf
Vasiliy <evtuth@gmail.com>
2022-12-20 12:18:51 +0300

    addded zadacha10 pravka vizuala

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -8,12 +8,12 @@
 Console.Write("Введите любое трехзначное число:");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if(a >= 100 && a<=999)
+if (a >= 100 && a <= 999)
 {
-int digit1 = a % 100;
-int digit2 = digit1 % 10;
-int digit3 = (digit1 - digit2) / 10;
-Console.WriteLine($"Ваше число: {digit3}");
+    int digit1 = a % 100;
+    int digit2 = digit1 % 10;
+    int digit3 = (digit1 - digit2) / 10;
+    Console.WriteLine($"Ваше число: {digit3}");
 }
 else
 {
