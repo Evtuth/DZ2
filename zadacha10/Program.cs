4c5de1605df6e7b6b6d0cf91acc2e7f4bd73d88a
Vasiliy <evtuth@gmail.com>
2022-12-20 12:14:48 +0300

    added zadacha10 second number

---
+++ zadacha10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите любое трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a >= 100 && a<=999)
+{
+int digit1 = a % 100;
+int digit2 = digit1 % 10;
+int digit3 = (digit1 - digit2) / 10;
+Console.WriteLine($"Ваше число: {digit3}");
+}
+else
+{
+    Console.WriteLine($"Неверное число!!!");
+}
